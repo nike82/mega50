3e3d9302b1a5cb1a080d09c586100721a4e0e109
Mikalai Bozin <Mikalai_Bozin@epam.com>
2023-05-13 21:39:16 +0400

    complete day 25

---
+++ day25/home_work.py
@@ -0,0 +1,48 @@
+from fpdf import FPDF
+import pandas as pd
+import glob
+from pathlib import Path
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
+file_paths = glob.glob("invoices/*.txt")
+
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+for file_path in file_paths:
+    pdf.add_page()
+    page_name = Path(file_path).stem.title()
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=page_name)
+
+pdf.output("PDFs/output.pdf")
+
+# for index, row in df.iterrows():
+#     pdf.add_page()
+#
+#     # Set the header
+#     pdf.set_font(family="Times", style="B", size=24)
+#     pdf.set_text_color(r=100, g=100, b=100)
+#     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+#
+#     for y in range(20, 298, 10):
+#         pdf.line(x1=10, y1=y, x2=200, y2=y)
+#
+#     # Set the footer
+#     pdf.ln(260)
+#     pdf.set_font(family="Times", style="I", size=8)
+#     pdf.cell(w=0, h=20, txt=row["Topic"], align="R", ln=1)
+#
+#     for i in range(row["Pages"] - 1):
+#         pdf.add_page()
+#
+#         # Set the footer
+#         pdf.ln(272)
+#         pdf.set_font(family="Times", style="I", size=8)
+#         pdf.cell(w=0, h=20, txt=row["Topic"], align="R", ln=1)
+#         for y in range(20, 298, 10):
+#             pdf.line(x1=10, y1=y, x2=200, y2=y)
+#
+# pdf.output("output_hw.pdf")
+
