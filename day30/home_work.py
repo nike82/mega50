2b87e2c06868016b1fff28f56700c3872a75d190
Mikalai Bozin <Mikalai_Bozin@epam.com>
2023-05-16 10:19:42 +0400

    weather api pandas complete day 30

---
+++ day30/home_work.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+df = pd.read_csv("dictionary.csv")
+
+
+@app.route("/")
+def home():
+    return render_template("home_work.html")
+
+
+@app.route("/api/v1/<word>")
+def about(word):
+    definition = df.loc[df["word"] == word]["definition"].squeeze()
+    return {"definition": definition, "word": word}
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
